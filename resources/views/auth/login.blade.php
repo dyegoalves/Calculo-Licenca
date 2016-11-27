836758633bf864750c785610b5f0480ef861e4b5
Dyego Alves <dyego.santos.alves@gmail.com>
2016-11-27 17:52:21 -0400

    btn abaixo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,10 +57,7 @@
               <br/>
               <div class="input-group">
                 <button type="submit" class="btn btn-marrom">Entrar</button>
-
-                  <a class="btn btn-marrom" href="http://">Perdeu sua senha?</a>
               </div>
-
                 <br/>
               <div class="input-group">
                 <a class="btn btn-marrom" href="http://">Perdeu sua senha?</a>
